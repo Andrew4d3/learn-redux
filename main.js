988b0f2b5ae836c5da67fe557d1c137e3a37c8eb
Andrew Davis <andrewdavis@wattopoststudio.com>
2016-07-31 17:34:57 -0400

    Implementing Store from Scratch https://egghead.io/lessons/javascript-redux-implementing-store-from-scratch

--- main.js
+++ main.js
@@ -11,7 +11,32 @@ const counter = (state = 0, action) => {
   }
 }
 
-const store  = Redux.createStore(counter)
+const createStore = (reducer) => {
+  let state
+  let listeners = []
+
+  const getState = () => state;
+
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
+  }
+
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter(l => l !== listener)
+    }
+  }
+
+  dispatch({})
+
+  return { getState, dispatch, subscribe }
+}
+
+
+// const store  = Redux.createStore(counter)
+const store = createStore(counter)
 
 const render = () => {
   document.body.innerText = store.getState()
